31a886bc29d1ee5a8b82dd35711bb3d1c2a9b60a
osman <silicasnd@gmail.com>
2025-10-20 18:02:11 +1300

    Add Render domain to ALLOWED_HOSTS

--- ScentSafe/settings.py
+++ ScentSafe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9*6a$=k#9$p%(%9k-f2!5@^&n$oxckaj)gfgti%l^i-+2lfd+t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scentsafe-20251019-142820.onrender.com'
 
 
 # Application definition
